3a306d9cddb2f2523e74a660d83eb1030c1fd289
Patrick Canfield <pscale01@gmail.com>
2020-05-09 17:50:23 -0700

    Set up webpack-dev-middleware

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,24 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: process.env.NODE_ENV,
   context: path.join(__dirname, './'),
   entry: './app/index.tsx',
   devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './app/index.html',
+      title: 'Your website'
+    })
+  ],
   output: {
-    path: path.join(__dirname, 'public'),
+    path: path.join(__dirname, 'dist'),
+    // use [name].bundle.js ?
     filename: 'bundle.js',
+    publicPath: '/',
+
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
